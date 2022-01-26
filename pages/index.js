0bb88a82bb193628a21d52912718ba1c0dc3b012
Renan Tiberto <renan.tiberto@gmail.com>
2022-01-25 21:53:33 -0300

    Aprendendo um pouco mais sobre SPA, useState e eventos do React

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,7 @@
-import appConfig from '../config.json'
-import { Box, Button, Text, TextField, Image } from '@skynexui/components'
-
-function GlobalStyle() {
-    return (
-        <style global jsx>
-            {`
-                * {
-                    margin: 0;
-                    padding: 0;
-                    box-sizing: border-box;
-                    list-style: none;
-                }
-
-                body {
-                    font-family: 'Ubuntu', sans-serif;
-                }
-
-                /* App fit Height */
-                html,
-                body,
-                #__next {
-                    min-height: 100vh;
-                    display: flex;
-                    flex: 1;
-                }
-
-                #__next {
-                    flex: 1;
-                }
-
-                #__next > * {
-                    flex: 1;
-                }
-                /* ./App fit Height */
-            `}
-        </style>
-    )
-}
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
+import appConfig from '../config.json';
 
 function Title(props) {
     const Tag = props.tag || 'h1'
@@ -70,11 +35,12 @@ function Title(props) {
 export default HomePage */
 
 export default function PaginaInicial() {
-    const username = 'renantiberto'
-
+    // const username = 'renantiberto'
+    const [username, setUsername] = React.useState('renantiberto')
+    const roteamento = useRouter();
+    
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex',
@@ -109,6 +75,11 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function(){
+                            infosDoEvento.preventDefault();
+                            roteamento.push('/chat');
+                            // window.location.href - "/chat"
+                        }}
                         styleSheet={{
                             display: 'flex',
                             flexDirection: 'column',
@@ -131,6 +102,14 @@ export default function PaginaInicial() {
                         </Text>
 
                         <TextField
+                            value={username}
+                            onChange={function(event){
+                                // Onde está o valor?
+                                const valor = event.target.value;
+                                // Trocar valor da variável,
+                                // Através do React e avise quem precisa
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
